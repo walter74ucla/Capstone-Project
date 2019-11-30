499fbcd469c65d5cf835958d2e69f5f6f9d060dc
Walter Sylvester <walter_sylvester@msn.com>
2019-11-30 13:00:39 -0700

    abandoned any API site. able to set up rapidapi site

---
+++ react-boxscore-client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BoxscoreContainer from './BoxscoreContainer';
+
+
+function App() {
+    
+  return(
+    <React.Fragment>
+        Some App text.
+        <h2>A heading</h2>
+        <BoxscoreContainer />
+    </React.Fragment>
+  )
+
+}
+
+
+export default App;
